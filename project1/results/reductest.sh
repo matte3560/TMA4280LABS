814a4f73bbeec36131663f10607e5e175e374250
Mathias <matte3560@gmail.com>
2017-03-05 14:01:31 +0100

    Add scaling tests with 16 and 32 processes.

--- project1/results/reductest.sh
+++ project1/results/reductest.sh
@@ -7,6 +7,11 @@ mpirun -n 4 ../reduc/zeta 8388608
 mv result.txt reduc_n4_s1.txt
 mpirun -n 8 ../reduc/zeta 8388608
 mv result.txt reduc_n8_s1.txt
+mpirun -n 16 ../reduc/zeta 8388608
+mv result.txt reduc_n16_s1.txt
+mpirun -n 32 ../reduc/zeta 8388608
+mv result.txt reduc_n32_s1.txt
+
 # reduc_rd size1
 mpirun -n 2 ../reduc/zeta_rd 8388608
 mv result.txt reduc_rd_n2_s1.txt
@@ -14,6 +19,11 @@ mpirun -n 4 ../reduc/zeta_rd 8388608
 mv result.txt reduc_rd_n4_s1.txt
 mpirun -n 8 ../reduc/zeta_rd 8388608
 mv result.txt reduc_rd_n8_s1.txt
+mpirun -n 16 ../reduc/zeta_rd 8388608
+mv result.txt reduc_rd_n16_s1.txt
+mpirun -n 32 ../reduc/zeta_rd 8388608
+mv result.txt reduc_rd_n32_s1.txt
+
 
 # reduc size2
 mpirun -n 2 ../reduc/zeta 16777216
@@ -22,6 +32,11 @@ mpirun -n 4 ../reduc/zeta 16777216
 mv result.txt reduc_n4_s2.txt
 mpirun -n 8 ../reduc/zeta 16777216
 mv result.txt reduc_n8_s2.txt
+mpirun -n 16 ../reduc/zeta 16777216
+mv result.txt reduc_n16_s2.txt
+mpirun -n 32 ../reduc/zeta 16777216
+mv result.txt reduc_n32_s2.txt
+
 # reduc_rd size2
 mpirun -n 2 ../reduc/zeta_rd 16777216
 mv result.txt reduc_rd_n2_s2.txt
@@ -29,3 +44,7 @@ mpirun -n 4 ../reduc/zeta_rd 16777216
 mv result.txt reduc_rd_n4_s2.txt
 mpirun -n 8 ../reduc/zeta_rd 16777216
 mv result.txt reduc_rd_n8_s2.txt
+mpirun -n 16 ../reduc/zeta_rd 16777216
+mv result.txt reduc_rd_n16_s2.txt
+mpirun -n 32 ../reduc/zeta_rd 16777216
+mv result.txt reduc_rd_n32_s2.txt
