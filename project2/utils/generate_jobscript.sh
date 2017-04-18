2602fcba15f172917e66152c19831834d7e34719
Mathias <matte3560@gmail.com>
2017-04-18 17:37:21 +0200

    Update jobscript generation script to select 2 nodes if num_procs*num_threads is greater than the number of CPUs on a single node.

--- project2/utils/generate_jobscript.sh
+++ project2/utils/generate_jobscript.sh
@@ -35,14 +35,24 @@ printf "Writing jobscript to ${FILE}\n"
 	printf "#PBS -W group_list=imf_lille-tma4280\n"
 	printf "#PBS -l walltime=00:05:00\n"
 
-	# Number of nodes based on desired number of processes
-	if [ $NUM_PROCS -le 20 ]; then
-		printf "#PBS -l select=1:ncpus=20"
-		if [ $NUM_PROCS -ne 0 ]; then
-			printf ":mpiprocs=${NUM_PROCS}"
-		fi
+	# Determine number of nodes
+	NODES=1
+	if [ $NUM_PROCS -gt 20 ]; then
+		NODES=2
 	else
-		printf "#PBS -l select=2:ncpus=20:mpiprocs=$(expr ${NUM_PROCS} / 2)"
+		if [ $(expr $NUM_PROCS \* $NUM_THR) -gt 20 ]; then
+			NODES=2
+		fi
+	fi
+
+	# Request resources
+	printf "#PBS -l select=${NODES}:ncpus=20"
+	if [ $NUM_PROCS -ne 0 ]; then
+		if [ $NODES -eq 1 ]; then
+			printf ":mpiprocs=${NUM_PROCS}"
+		else
+			printf ":mpiprocs=$(expr ${NUM_PROCS} / 2)"
+		fi
 	fi
 	# Append number of threads if provided and end the line
 	if [ $NUM_THR -ne 0 ]; then
